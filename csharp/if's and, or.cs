36797aef66329abaf71870ed884e8b36972a28ea
namg8888 <52068462+namg8888@users.noreply.github.com>
2025-01-05 21:23:33 +0900

    and 와 or을 사용한 if문

---
+++ csharp/if's and, or.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace MyCompiler {
+    class Program {
+        public static void Main(string[] args) {
+        //1. a를 입력한다
+        //2. a가 20보다 작고 3의 배수이면 "find number!" 출력
+        //3. a가 20보다 작거나 3의배수이면 "similar number!"
+        //4. a가 20보다 크면 "not number :(" 출력
+        string input = Console.ReadLine();
+        int a;
+        string a1 = input.Substring(0);
+        a = int.Parse(a1);
+        
+        if(a<20 && a%3==0){
+        Console.WriteLine("find number!");
+        }
+            else if(a<20 || a%3==0){
+        Console.WriteLine("similar number!");        
+            }
+            else{
+        Console.WriteLine("not number");  
+            }
+        }
+    }
+}
