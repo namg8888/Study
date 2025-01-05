a7f65c17e1988fc062ec605470b2ebafe2841c11
namg8888 <52068462+namg8888@users.noreply.github.com>
2025-01-05 20:35:09 +0900

    고양이 만들기

---
+++ csharp/baekjoon/10171.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace MyCompiler {
+    class Program {
+        public static void Main(string[] args) {
+        //\    /\
+        // )  ( ')
+        //(  /  )
+        // \(__)|
+        Console.WriteLine("\\    /\\");
+        Console.WriteLine(" )  ( ')");
+        Console.WriteLine("(  /  )");
+        Console.WriteLine(" \\(__)|");
+
+        }
+    }
+}
