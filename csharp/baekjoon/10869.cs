f0a0834b40919a701562a9e5a495b423e32a3526
namg8888 <52068462+namg8888@users.noreply.github.com>
2025-01-05 20:47:07 +0900

    문제 풀이 관련 상세 주석 추가

--- csharp/baekjoon/10869.cs
+++ csharp/baekjoon/10869.cs
@@ -3,14 +3,17 @@ using System;
 namespace MyCompiler {
     class Program {
         public static void Main(string[] args) {
-        string input = Console.ReadLine();
-        int space = input.IndexOf(' ');
+        
+        //
+
+        string input = Console.ReadLine();//프로그램 입력을 활성화하기
+        int space = input.IndexOf(' ');// 프로그램 입력에서 공백을 제외
         int a, b;
-        string a1 = input.Substring(0, space);  // 첫 번째 숫자
-        string b1 = input.Substring(space + 1);  // 두 번째 숫자
-        a = int.Parse(a1);
-        b = int.Parse(b1);
-        Console.WriteLine($"{a+b}");
+        string a1 = input.Substring(0, space);  // 첫 번째 숫자 입력
+        string b1 = input.Substring(space + 1);  // 두 번째 숫자 입력
+        a = int.Parse(a1);// 입력 받은 첫번째 숫자를 int로 변환
+        b = int.Parse(b1);// 입력 받은 두번째 숫자를 int로 변환
+        Console.WriteLine($"{a+b}"); 
         Console.WriteLine($"{a-b}");
         Console.WriteLine($"{a*b}");
         Console.WriteLine($"{a/b}");
