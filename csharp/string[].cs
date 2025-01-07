e0b1170c7cd51c5c5f62dee9e7d2ebcd8c7473e1
namg8888 <52068462+namg8888@users.noreply.github.com>
2025-01-07 21:59:50 +0900

    이중배열

---
+++ csharp/string[].cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyCompiler {
+    class Program {
+        public static void Main(string[] args) {
+        char[] a = {'a', 'p', 'p', 'l', 'e'};
+        Console.WriteLine(a[1]);
+
+        string[] b = {"apple", "bear", "cat", "dead"};
+        Console.WriteLine(b[1]);
+        
+}
+}
+}
