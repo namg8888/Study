7b693390ad10c79a12edc63cc9ea571082033c93
namg8888 <52068462+namg8888@users.noreply.github.com>
2025-01-10 20:25:41 +0900

    완료

--- 임시.cs
+++ 임시.cs
@@ -10,8 +10,8 @@ namespace MyCompiler {
         int a = int.Parse(parts[0]);//첫번째 배열
         int b = int.Parse(parts[1]);//두번째 배열
         int c = int.Parse(parts[2]);//세번째 배열
-        int d = int.Parse(parts[2]);//네번째 배열
-        int e = int.Parse(parts[2]);//다섯번째 배열
+        int d = int.Parse(parts[3]);//네번째 배열
+        int e = int.Parse(parts[4]);//다섯번째 배열
         int f = 1;//가장 작은 수
         int g = 1;//두번째로 작은 수
         
