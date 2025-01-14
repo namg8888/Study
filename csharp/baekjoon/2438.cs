5ea5c509f1a63c3d2c84fa9b7448e1c898aac2b5
namg8888 <52068462+namg8888@users.noreply.github.com>
2025-01-14 10:50:12 +0900

    *쌓기 완성

--- csharp/baekjoon/2438.cs
+++ csharp/baekjoon/2438.cs
@@ -6,10 +6,14 @@ namespace MyCompiler {
         string a = Console.ReadLine();
         int n = int.Parse(a);
         char b = '*';
-        for(int i = 0; i < n; i++){     
-        Console.Write($"{b}");    
-        Console.WriteLine($"{b}");    
-        }
-        }
+        for(int i = 1; i <= n; i++){     
+        for(int z = 0; z < i; z++){
+        Console.Write($"{b}");
+            
+        }//이차 for
+            Console.WriteLine();
+        }//일차 for
+        
+            }
     }
 }
