ea7e911ce4d3f311221d119e00583a99fd367761
namg8888 <52068462+namg8888@users.noreply.github.com>
2025-01-20 17:55:54 +0900

    * 역으로 쌓기

--- csharp/baekjoon/2439.cs
+++ csharp/baekjoon/2439.cs
@@ -6,16 +6,17 @@ namespace MyCompiler {
         string a = Console.ReadLine();
         int n = int.Parse(a);
         char b = '*';
-        for(int i = 1; i <= n; i++){     
-        for(int x = 0; x < n - i; x++){
-        Console.Write(" ");    
+        for(int i = 1; i <= n; i++){//1부터 n번까지 반복
+     
+        for(int x = 0; x < n - i; x++){//n-1만큼 반복
+        Console.Write(" ");//공백 출력    
         }        
-        for(int z = 0; z < i; z++){
-        Console.Write(b);
+        for(int z = 0; z < i; z++){//i번 반복
+        Console.Write(b);//별 출력
             
         }
 
-        Console.WriteLine();
+        Console.WriteLine();//줄바꿈
         }
         
             }
