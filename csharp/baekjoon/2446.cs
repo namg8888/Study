ed187f369a391419d983739166b4d7f27994cec0
namg8888 <52068462+namg8888@users.noreply.github.com>
2025-01-20 19:52:01 +0900

    모래시계 모양으로 *쌓기

---
+++ csharp/baekjoon/2446.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace MyCompiler {
+    class Program {
+        public static void Main(string[] args) {
+        string a = Console.ReadLine();
+        int n = int.Parse(a);
+        char b = '*';
+        for(int i = 1; i <= n; i++){//1부터 n번까지 반복
+     
+        for(int x = 0; x < n - i; x++){//n-1만큼 반복
+        Console.Write(" ");//공백 출력    
+        }        
+        for(int z = 0; z < i; z++){//i번 반복
+        Console.Write(b);//별 출력
+        Console.Write(" ");//공백 출력            
+        }
+
+        Console.WriteLine();//줄바꿈
+        }
+        
+            }
+    }
+}
