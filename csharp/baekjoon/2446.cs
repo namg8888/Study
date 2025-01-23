94191b23e03d88bd81c55449ae2462787df0d762
namg8888 <52068462+namg8888@users.noreply.github.com>
2025-01-23 12:11:16 +0900

    모래시계 별찍기

--- csharp/baekjoon/2446.cs
+++ csharp/baekjoon/2446.cs
@@ -6,14 +6,26 @@ namespace MyCompiler {
         string a = Console.ReadLine();
         int n = int.Parse(a);
         char b = '*';
-        for(int i = 1; i <= n; i++){//1부터 n번까지 반복
+            
+        for(int i = 0; i < n; i++){//0부터 n-1번까지 반복
      
-        for(int x = 0; x < n - i; x++){//n-1만큼 반복
+        for(int x = 0; x < i; x++){
         Console.Write(" ");//공백 출력    
         }        
-        for(int z = 0; z < i; z++){//i번 반복
-        Console.Write(b);//별 출력
-        Console.Write(" ");//공백 출력            
+        for(int z = 0; z < 2 * (n - i) - 1; z++){
+        Console.Write(b);//별 출력            
+        }
+
+        Console.WriteLine();//줄바꿈
+        }
+            
+        for(int i = 1; i < n; i++){//1부터 n-1번까지 반복
+     
+        for(int x = 0; x < n - i - 1; x++){
+        Console.Write(" ");//공백 출력    
+        }        
+        for(int z = 0; z < 2 * i + 1; z++){
+        Console.Write(b);//별 출력            
         }
 
         Console.WriteLine();//줄바꿈
